d3268943a061407243bdf202a83f93698d20b922
kevin schuchard <schuchard.kevin@gmail.com>
2018-11-05 06:26:06 -0700

    binding examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'mat';
+  fast = interval(1000).pipe(takeWhile((val) => val <= 10));
+  slow = interval(2000).pipe(takeWhile((val) => val <= 5));
 }
